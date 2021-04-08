26b50a7618458ca51f9cefa12d599bb2087b0f82
thsdmfwns <son2044@gmail.com>
2021-04-08 15:51:37 +0900

    탭버튼 기본생성

---
+++ lib/page/widget/tab_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class TabButton extends StatefulWidget {
+  final double height;
+  final double width;
+  final Color checkedColor;
+  final Color notCheckColor;
+  @required final void Function() onPressed;
+  @required final Widget child;
+  @required final bool isChecked;
+
+  const TabButton({this.height, this.width, this.isChecked, this.checkedColor, this.notCheckColor, this.onPressed, this.child});
+  @override
+  _TabButtonState createState() => _TabButtonState();
+}
+
+class _TabButtonState extends State<TabButton> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onPressed,
+      child: Container(
+        height: widget.height ?? null,
+        width: widget.width ?? null,
+        color: widget.isChecked ?
+              widget.checkedColor ?? Colors.blueAccent :
+              widget.notCheckColor ?? Colors.grey,
+        child: widget.child,
+      ),
+    );
+  }
+}
